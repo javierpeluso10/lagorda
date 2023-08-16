b51269b890fba5713d41e5ed9364be94d80382c6
Javier <106279685+javierpeluso10@users.noreply.github.com>
2023-08-15 21:33:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("buttonId")
+
+const revelarNombre = () => {
+    alert("Mi gorda hermosa se llama Tamara, y le comeria todo el coño")
+}
+
+button.addEventListener("click", revelarNombre)
