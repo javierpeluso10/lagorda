f146a4531ffd86aaf69bf4cbbf8827c22d25f764
Javier <106279685+javierpeluso10@users.noreply.github.com>
2023-08-15 21:33:25 -0300

    Delete prueba directory

--- prueba/script.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById("buttonId")
-
-const revelarNombre = () => {
-    alert("Mi gorda hermosa se llama Tamara, y le comeria todo el coño")
-}
-
-button.addEventListener("click", revelarNombre)
